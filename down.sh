efe26bb29e82d8a4b601054bc590232d6bbfe4f8
Josh <fetmar@gmail.com>
2018-05-23 12:29:27 -0400

    updarte config.toml

--- down.sh
+++ down.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# This script brings up the chaos node in kubernetes.
+# This script removes everything that was insallted in kubernetes.
 
 kubectl delete configmap tendermint-config-genesis
 kubectl delete configmap tendermint-config-priv-validator
-#kubectl delete configmap tendermint-config-node-key
+kubectl delete configmap tendermint-config-toml
 kubectl delete -f chaosnode-deployment.yaml
 kubectl delete -f chaosnode-service.yaml
 kubectl delete -f tendermint-deployment.yaml
--- up.sh
+++ up.sh
@@ -2,14 +2,21 @@
 
 # This script brings up the chaos node in kubernetes.
 
-
-#cp $HOME/.tendermint/config/genesis.json $HOME/.tendermint/config/genesis.bak
-#empty_hash=$(
-#    docker-compose run --rm --no-deps chaosnode --echo-empty-hash |\
-#    tr -d '\r'
-#)
-#jq ".app_hash=\"$empty_hash\"" $HOME/.tendermint/config/genesis.bak > $HOME/.tendermint/config/genesis.json
-#rm $HOME/.tendermint/config/genesis.bak
+# gnu sed is required
+sed=sed
+sed --version > /dev/null 2>&1
+if [ $? != 0 ]; then
+    which gsed >/dev/null
+    if [ $? != 0 ]; then
+        (
+            >&2 echo "You have a broken version of sed, and gsed is not installed"
+            >&2 echo "This is common on OSX. Try `brew install gnu-sed`"
+            exit 1
+        )
+    fi
+    sed=gsed
+    echo "using $sed as sed"
+fi
 
 # install chaosnode and noms
 kubectl apply -f chaosnode-deployment.yaml
@@ -53,7 +60,21 @@ cp $HOME/.tendermint/config/genesis.json $HOME/.tendermint/config/genesis.bak
 jq ".app_hash=\"$emptyHash\"" $HOME/.tendermint/config/genesis.bak > $HOME/.tendermint/config/genesis.json
 rm $HOME/.tendermint/config/genesis.bak
 
+# update config.toml
+cp $HOME/.tendermint/config/config.toml $HOME/.tendermint/config/config.bak 
+$sed -E \
+    -e '/^proxy_app/s|://[^:]*:|://chaosnode-service:|' \
+    -e '/^create_empty_blocks_interval/s/[[:digit:]]+/10/' \
+    -e '/^create_empty_blocks\b/{
+            s/true/false/
+            s/(.*)/# \1/
+            i # tendermint respects create_empty_blocks *OR* create_empty_blocks_interval
+        }' \
+    $HOME/.tendermint/config/config.bak > $HOME/.tendermint/config/config.toml
+rm $HOME/.tendermint/config/config.bak
+
 # make configmaps for tendermint
+kubectl create configmap tendermint-config-toml --from-file=$HOME/.tendermint/config/config.toml
 kubectl create configmap tendermint-config-genesis --from-file=$HOME/.tendermint/config/genesis.json
 kubectl create configmap tendermint-config-priv-validator --from-file=$HOME/.tendermint/config/priv_validator.json 
 
