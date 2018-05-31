1a2cd5e9b9324f6f5beffb3a9b7bc08bfcfa51d9
Josh <fetmar@gmail.com>
2018-05-31 10:44:04 -0400

    templatize subdomains

---
+++ aws/down.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script removes everything that was insallted in kubernetes.
+
+kubectl delete -f manifests/tendermint-config-init.yaml
+kubectl delete configmap tendermint-config-genesis
+kubectl delete configmap tendermint-config-priv-validator
+kubectl delete configmap tendermint-config-toml
+kubectl delete -f manifests/chaosnode-deployment.yaml
+kubectl delete -f manifests/chaosnode-service.yaml
+kubectl delete -f manifests/tendermint-deployment.yaml
+kubectl delete -f manifests/tendermint-service.yaml
+kubectl delete -f manifests/noms-deployment.yaml
+kubectl delete -f manifests/noms-service.yaml
+
+# This should be gone already, but just incase it isn't.
+kubectl delete -f manifests/empty-hash-get-job.yaml 2>/dev/null
+
+# This will delete the volumes, which might not be what you want.
+kubectl delete -f manifests/volumes.yaml 2>/dev/null
+
+kubectl delete -f manifests/tendermint-ingress.yaml
+
+kubectl delete -f ../manifests/nginx-ingress/mandatory.yaml
+kubectl delete -f ../manifests/nginx-ingress/service-l4.yaml
+kubectl delete -f ../manifests/nginx-ingress/patch-configmap-l4.yaml
+
+
+exit 0
--- aws/subdomain.sh
+++ aws/subdomain.sh
@@ -3,7 +3,7 @@
 # Configure kops
 usage() {
     echo "Usage"
-    echo "SD=dev.cluster.ndau.tech ./subdomain.sh"
+    echo "SD=cluster.ndau.tech ./subdomain.sh"
 }
 
 if [ -z "$SD" ]; then
@@ -12,19 +12,20 @@ if [ -z "$SD" ]; then
     exit 1
 fi
 
+# Get the parent domain
+PD=$(sed "s/[^\.]*\.//" <<< $SD)
 
 # make a uuid for a request
 ID=$(uuidgen)
 
 # create a new hosted zone and get nameservers
 NS=$(aws route53 create-hosted-zone \
-        --name cluster.ndau.tech \
+        --name $SD \
         --caller-reference $ID |\
     jq .DelegationSet.NameServers)
 
 # get hosted zone for the parent domain
-parent_HZ=$(aws route53 list-hosted-zones |\
-    jq -r '.HostedZones[] | select(.Name=="ndau.tech.") | .Id')
+parent_HZ=$(aws route53 list-hosted-zones | jq -r ".HostedZones[] | select(.Name==\"${PD}.\") | .Id")
 
 # sed all the things
 cp subdomain.template subdomain.json
@@ -34,23 +35,9 @@ sed -i "s/DNS_3/$(jq -r '.[2]' <<< $NS)/g" subdomain.json
 sed -i "s/DNS_4/$(jq -r '.[3]' <<< $NS)/g" subdomain.json
 sed -i "s/PARENT_DOMAIN/${PARENT_DOMAIN}/g" subdomain.json
 
+# give a cname to the hosted zone
 aws route53 change-resource-record-sets \
-    --hosted-zone-id $parent_HZ \
+    --hosted-zone-id ${parent_HZ} \
     --change-batch file://subdomain.json
-
-
-aws s3api create-bucket \
-    --bucket ${BUCKET} \
-    --region us-east-1
-aws s3api put-bucket-versioning --bucket ${BUCKET} --versioning-configuration Status=Enabled
-
-export NAME=dev.cluster.ndau.tech
-export KOPS_STATE_STORE=s3://${BUCKET}
-
-AZ=us-east-1a
-
-kops create cluster --zones ${AZ} ${NAME}
-
-
 
 
