c0d198dfe97a1acf7ec459de71c0b7349f950b06
Josh <fetmar@gmail.com>
2018-05-31 10:36:11 -0400

    change comments

--- aws/build.sh
+++ aws/build.sh
@@ -3,10 +3,7 @@
 # This script builds the images.
 # This script is fragile and needs to be kept in sync with chaosnode build instructions.
 # This should probably be moved to chaosnode and kept up to date there.
-# Note, this builds to minikube's docker. In the event these builds fail becasuse 
-# the machine is out of space, you may `minikube delete` which will completely remove
-# the virtual machine. Normally a build process would push to a container repository 
-# and not a single virtual machine.
+# This pushes to ECR 
 
 GREEN='\033[0;32m'
 NC='\033[0m' # no color
