abd99f3d1aab6668d23cffc7196577206784dd3b
Josh <fetmar@gmail.com>
2018-08-10 10:39:46 -0400

    Refactor addy for better testing. Try linting and go testing in circleci.

--- addy/main.go
+++ addy/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -11,8 +12,7 @@ import (
 	"github.com/tendermint/ed25519"
 )
 
-func main() {
-
+func readStdin() ([]byte, error) {
 	// Read from stdin
 	stat, err := os.Stdin.Stat()
 	if err != nil {
@@ -21,32 +21,32 @@ func main() {
 
 	// is not piped
 	if stat.Mode()&os.ModeCharDevice != 0 {
-		fmt.Fprintln(os.Stderr, "Usage: echo \"base64-Ed25519-priv-key\" | ./address")
-		os.Exit(1)
+		return nil, errors.New("Usage: echo \"base64-Ed25519-priv-key\" | ./address")
 	}
 
 	// no input
 	if stat.Size() <= 0 {
-		fmt.Fprintln(os.Stderr, "Warning, no input")
-		os.Exit(0)
+		return nil, errors.New("Input empty")
 	}
 
 	jsonB64, err := ioutil.ReadAll(os.Stdin) // not expecting big inputs
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading from stdin: %v", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("Error reading from stdin: %v", err)
 	}
+
+	return jsonB64, nil
+}
+
+func makeAddressHash(jsonB64 []byte) (string, error) {
 
 	// decode b64 to regular bytes
 	jsonB := make([]byte, base64.StdEncoding.DecodedLen(len(jsonB64)))
 	n, err := base64.StdEncoding.Decode(jsonB, jsonB64)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error decoding base64: %v.", err)
-		os.Exit(1)
+		return "", fmt.Errorf("error decoding base64: %v", err)
 	}
 	if n != 64 {
-		fmt.Fprintf(os.Stderr, "Expecting 64 bytes. Got %v.", n)
-		os.Exit(1)
+		return "", fmt.Errorf("expecting 64 bytes. Got %v", n)
 	}
 
 	// actually make the id
@@ -54,8 +54,25 @@ func main() {
 	copy(privKeyBytes[:], jsonB)
 	pubBytes := *ed25519.MakePublicKey(&privKeyBytes)
 	pubHash := sha256.Sum256(pubBytes[:])
+	trunc := pubHash[:20]
+	hex := hex.EncodeToString(trunc)
+
+	return hex, nil
+}
+
+func main() {
+
+	// Read from stdin
+	jsonB64, err := readStdin()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read from stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	// decode b64 to regular bytes. actually make the id
+	hex, err := makeAddressHash(jsonB64)
 
 	// output the hexbytes to stdout
-	fmt.Printf(hex.EncodeToString(pubHash[:20]))
+	fmt.Printf(hex)
 
 }
