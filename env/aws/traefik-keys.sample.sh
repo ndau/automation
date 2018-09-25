84effec574db09393047146671ded6bd7aa421b8
Josh <fetmar@gmail.com>
2018-09-25 00:37:48 -0400

    add traefik

---
+++ env/aws/traefik-keys.sample.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# disable shellcheck rule because this variable is used in another script
+# shellcheck disable=2034
+AWS_ACCESS_KEY_ID=your_access_key
+AWS_SECRET_ACCESS_KEY=your_secret_access_key
