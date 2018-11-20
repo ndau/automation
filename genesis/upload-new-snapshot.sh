227695aa03274bfe74174d96ca487e5865189091
Josh <fetmar@gmail.com>
2018-11-20 10:38:02 -0500

    add missing images

--- genesis/upload-new-snapshot.sh
+++ genesis/upload-new-snapshot.sh
@@ -12,6 +12,35 @@ DATE=$(date '+%Y-%m-%dT%H-%M-%SZ')
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TEMP_DIR="$DIR/tmp-$DATE"
 
+# if there's no NDAUNODE_TAG specified, use these
+if [ -z "$NDAUNODE_TAG" ]; then
+	NDAUNODE_TAG=$(git ls-remote https://github.com/oneiro-ndev/ndau |\
+        grep 'refs/heads/master' | \
+        awk '{{print $1}}' | \
+        cut -c1-7)
+	if [ -z "$NDAUNODE_TAG" ]; then
+		errcho "Couldn't fetch ndau's master sha"
+		exit 1
+	fi
+	verrcho "Using chaos master sha: $NDAUNODE_TAG"
+fi
+
+if [ -z "$CHAOSNODE_TAG" ]; then
+	CHAOSNODE_TAG=$(git ls-remote https://github.com/oneiro-ndev/chaos |\
+        grep 'refs/heads/master' | \
+        awk '{{print $1}}' | \
+        cut -c1-7)
+	if [ -z "$CHAOSNODE_TAG" ]; then
+		errcho "Couldn't fetch chaos's master sha"
+		exit 1
+	fi
+	verrcho "Using chaos master sha: $CHAOSNODE_TAG"
+fi
+
+# Use these images
+CHAOS_IMAGE=578681496768.dkr.ecr.us-east-1.amazonaws.com/chaosnode:$CHAOSNODE_TAG
+NDAU_IMAGE=578681496768.dkr.ecr.us-east-1.amazonaws.com/ndaunode:$NDAUNODE_TAG
+
 # configy things
 CHAOS_NOMS=$NDAUHOME/chaos/noms
 NDAU_NOMS=$NDAUHOME/ndau/noms
