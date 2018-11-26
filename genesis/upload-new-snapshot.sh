90a79964a3b186bb9c75964198a32c9ad19bbc8c
John Greenland <jsgjnr@yahoo.com>
2018-11-26 14:20:49 -0500

    Upload script changes to fix errors

--- genesis/upload-new-snapshot.sh
+++ genesis/upload-new-snapshot.sh
@@ -12,6 +12,20 @@ DATE=$(date '+%Y-%m-%dT%H-%M-%SZ')
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TEMP_DIR="$DIR/tmp-$DATE"
 
+if [ "$1" == "-v" ]; then
+ VERBOSE=true
+fi
+
+# echo to stderr
+errcho() {
+ >&2 echo "$@"
+}
+
+# verbose errecho
+verrcho() {
+ if $VERBOSE; then errcho "$@"; fi
+}
+
 # if there's no NDAUNODE_TAG specified, use these
 if [ -z "$NDAUNODE_TAG" ]; then
 	NDAUNODE_TAG=$(git ls-remote https://github.com/oneiro-ndev/ndau |\
@@ -22,7 +36,7 @@ if [ -z "$NDAUNODE_TAG" ]; then
 		errcho "Couldn't fetch ndau's master sha"
 		exit 1
 	fi
-	verrcho "Using chaos master sha: $NDAUNODE_TAG"
+	verrcho "Using ndau master sha: $NDAUNODE_TAG"
 fi
 
 if [ -z "$CHAOSNODE_TAG" ]; then
@@ -42,28 +56,28 @@ CHAOS_IMAGE=578681496768.dkr.ecr.us-east-1.amazonaws.com/chaosnode:$CHAOSNODE_TA
 NDAU_IMAGE=578681496768.dkr.ecr.us-east-1.amazonaws.com/ndaunode:$NDAUNODE_TAG
 
 # configy things
-CHAOS_NOMS=$NDAUHOME/chaos/noms
-NDAU_NOMS=$NDAUHOME/ndau/noms
+CHAOS_NOMS=~/go/src/github.com/attic-labs/noms/cmd/noms/s3/data.chaos
+NDAU_NOMS=~/go/src/github.com/attic-labs/noms/cmd/noms/s3/data.ndau
 
 
 # let's get started
 
 # reset files
-rm -rf "$TEMP_DIR"
+mkdir -p "$TEMP_DIR"
 
 # get hashes
-CHAOS_HASH=$(docker run \
-	--name="chaosnode-hash-$RND" \
-	--network="host" \
-	--mount src="$NDAUHOME",target="$NDAUHOME",type=bind \
-	$CHAOS_IMAGE \
-	-echo-hash --spec http://$LH:$CHAOS_NOMS_PORT >&1 )
-NDAU_HASH=$(docker run \
-	--name="ndaunode-hash-$RND" \
-	--network="host" \
-	--mount src="$NDAUHOME",target="$NDAUHOME",type=bind \
-	$NDAU_IMAGE \
-	 -echo-hash --spec http://$LH:$NDAU_NOMS_PORT >&1 )
+# CHAOS_HASH=$(docker run \
+#	--name="chaosnode-hash-$RND" \
+#	--network="host" \
+	# --mount src="$NDAUHOME",target="$NDAUHOME",type=bind \
+	# $CHAOS_IMAGE \
+	# -echo-hash --spec http://$LH:$CHAOS_NOMS_PORT >&1 )
+# NDAU_HASH=$(docker run \
+	# --name="ndaunode-hash-$RND" \
+	# --network="host" \
+	# --mount src="$NDAUHOME",target="$NDAUHOME",type=bind \
+	# $NDAU_IMAGE \
+	#  -echo-hash --spec http://$LH:$NDAU_NOMS_PORT >&1 )
 
 # zip up the noms databases
 (
@@ -85,4 +99,5 @@ aws s3 cp "$TEMP_DIR"/latest.txt s3://ndau-snapshots/latest.txt
 aws s3 cp "$TEMP_DIR"/ndau-noms.tgz s3://ndau-snapshots/"$DATE"/ndau-noms.tgz
 aws s3 cp "$TEMP_DIR"/chaos-noms.tgz s3://ndau-snapshots/"$DATE"/chaos-noms.tgz
 
+rm -rf "$TEMP_DIR"
 exit 0
